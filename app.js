327faa29e65d7df1b7dc351eef9b418375ddff6c
El-boujamai <ouardiaelboujamaai70@gmail.com>
2026-01-02 02:03:49 +0100

    ajout avec erreurs

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('<h1>Hello DevOps !</h1><p>Le pipeline fonctionne.</p>');
 });
-app.listen(3000, () => console.log('Serveur prêt sur le port 3000'));
+app.listen(3000, () => console.log('Serveur prêt sur le port 3000
